ac940d06f02d6b87c2d09fddd9684c39d756c39d
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-01-10 10:54:31 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -2,14 +2,14 @@
 
 # Define the source and destination paths
 SOURCE="/var/lib/jenkins/workspace/pipeline1/target/webapp-0.1.war"
-DESTINATION="/opt/tomcat/webapps/"
+DESTINATION="/opt/tomcat9/webapps/"
 
 # Copy the WAR file to the Tomcat webapps directory
 echo "Copying WAR file to Tomcat webapps directory..."
 cp "$SOURCE" "$DESTINATION"
 
 # Change to the Tomcat bin directory
-cd /opt/tomcat/bin || exit
+cd /opt/tomcat9/bin || exit
 
 # Start the Tomcat server
 echo "Starting Tomcat server..."
